ad074b73a09db01c1aa24f2f932a93e7e01da767
AlgorithmXplorer <tanrininkirbaci36@gmail.com>
2025-08-15 23:19:39 +0300

    A file created for the all file functions and the url maker func that in Immersive_Products class have been finished

--- Immersive_Products.py
+++ Immersive_Products.py
@@ -1,5 +1,23 @@
 
 import requests as rq
+from parameter_bringer import Immersive_Products_parameters
 import json as js
+
+class Immersive_Products:
+    def __init__(self,parameters):
+        self.params = parameters
+        self.url = None
+        self.Datas = None
+    
+    def url_maker(self):
+        with open("api_key/api.json","r+",encoding="utf-8") as file:
+            datas = js.load(file)
+
+        str_params = [f"{name}={param}" for name,param in self.params.items()]
+        self.url = datas["url"] + f"api_key={datas['api_key']}&" + "&".join(str_params)  
+
+params = Immersive_Products_parameters()
+product = Immersive_Products(parameters=params)
+product.url_maker()
 
 
--- parameter_bringer.py
+++ parameter_bringer.py
@@ -1,80 +1,26 @@
-import os
-import pygetwindow 
-import time
-import json as js
+from file_funcs import domain_setter
+from file_funcs import param_taker
+countrys_url = "https://serpapi.com/google-domains"
+    
 
-def domain(code:str):
-    with open("google-domains.json","r+",encoding="utf-8") as file:
-        countrys = js.load(file)
-    for country in countrys:
-        if code == country["country_code"]:
-            return country["domain"]
-
-def param_taker(questions:list):
-    #* the question file creating and writing of questions
-    questions = [ques + ":" for ques in questions]
-    with open("questions.txt","a",encoding="utf-8") as file:
-        for question in questions:
-            file.write(question+"\n")
-        file.write("\nPlease write your filters correctly and dont forget save and close the file.\nAlso don't change any line in this file  except filter lines.")
-    
-    def data_taker():
-        while True:
-            windows:str = pygetwindow.getActiveWindowTitle()
-            windows = windows.split(" - ")
-            if "questions.txt" in windows or "● questions.txt" in windows:
-                time.sleep(0.25)
-            else:
-                break
-        with open("questions.txt" ,"r+" , encoding="Utf-8") as file:
-            question_lines = file.readlines()[:len(questions)]
-        return question_lines
-    
-    os.system("questions.txt")
-    
-    #* data clearing
-    question_lines = data_taker()
-    question_lines = [line.split(":")[-1] for line in question_lines]
-    question_lines = [line[:-1] for line in question_lines]
-    question_lines = [line.strip(" ") for line in question_lines]
-    question_lines = [line if line != "" else None for line in question_lines ]
-    os.remove("questions.txt")
-    return question_lines
-
-countrys = "https://serpapi.com/google-domains"
-    
 def Immersive_Products_parameters() -> dict:
-    questions = ["What do you want to search ",f"Which country will be use(country codes-dont write dot-;{countrys}) "]
+    questions = ["What do you want to search ",f"Which country will be use(country codes-dont write dot-;{countrys_url}) "]
     answers = param_taker(questions=questions)
     params = {
         "q":"+".join(answers[0].split(" ")),
-        "google_domain":domain(answers[1].lower()),
+        "google_domain":domain_setter(answers[1].lower()),
         "engine":"google",
         "safe":"active",
         "device":"desktop"
         }
     return params
 
-"""
-default:
--engine
--device
--safe
--api_key
-
-optional
--q
--google_domain ( normally i will use gl and hl parameters but google domain is more simple)
-"""
-
-
-
 def Jobs_parameters() -> dict:
-    questions = ["Which job do you want to search ",f"Which country will be use(countrys codes-dont write dot-;{countrys}) "]
+    questions = ["Which job do you want to search ",f"Which country will be use(countrys codes-dont write dot-;{countrys_url}) "]
     answers = param_taker(questions=questions)
     params = {
         "q":"+".join(answers[0].split(" ")),
-        "google_domain":domain(answers[1].lower()),
+        "google_domain":domain_setter(answers[1].lower()),
         "engine":"google_jobs",
         "safe":"active",
         "device":"desktop"
@@ -83,11 +29,11 @@ def Jobs_parameters() -> dict:
 
 
 def Grammar_Check_parameters() -> dict:
-    questions = ["What grammer do you want to check",f"Which country will be use(countrys codes-dont write dot-;{countrys})"]
+    questions = ["What grammer do you want to check",f"Which country will be use(countrys codes-dont write dot-;{countrys_url})"]
     answers = param_taker(questions=questions)
     params = {
         "q": "+".join(answers[0].split(" ")) +".+grammar+check",
-        "google_domain":domain(answers[1].lower()),
+        "google_domain":domain_setter(answers[1].lower()),
         "engine":"google",
         "device":"desktop"
         }
@@ -95,11 +41,11 @@ def Grammar_Check_parameters() -> dict:
 
 
 def Nutrition_Information_parameters() -> dict:
-    questions = ["What do you want to search",f"Which country will be use(countrys codes-dont write dot-;{countrys})"]
+    questions = ["What do you want to search for subject matter",f"Which country will be use(countrys codes-dont write dot-;{countrys_url})"]
     answers = param_taker(questions=questions)
     params = {
         "q": "+".join(answers[0].split(" ")),
-        "google_domain":domain(answers[1].lower()),
+        "google_domain":domain_setter(answers[1].lower()),
         "engine":"google",
         "safe":"active",
         "device":"desktop"
@@ -109,29 +55,29 @@ def Nutrition_Information_parameters() -> dict:
 
 
 def Popular_Destinations_parameters() -> dict:
-    questions = ["What do you want to search for a trip",f"Which country will be use(countrys codes-dont write dot-;{countrys})"]
+    questions = ["What do you want to search for a trip",f"Which country will be use(countrys codes-dont write dot-;{countrys_url})"]
     answers = param_taker(questions=questions)
     params = {
         "q": "+".join(answers[0].split(" ")) +"+Destinations",
-        "google_domain":domain(answers[1].lower()),
+        "google_domain":domain_setter(answers[1].lower()),
         "engine":"google",
         "safe":"active",
         "device":"desktop"
         }
     return params
-#t destination parametreleri ve soruları ayarlanıcak
+
+
+
 def Courses_parameters() -> dict:
-    questions = ["What course do you want to search ",f"Which country will be use(countrys codes-dont write dot-;{countrys})"]
+    questions = ["What course do you want to search ",f"Which country will be use(countrys codes-dont write dot-;{countrys_url})"]
     answers = param_taker(questions=questions)
     params = {
         "q": "+".join(answers[0].split(" ")) +"+course",
-        "google_domain":domain(answers[1].lower()),
+        "google_domain":domain_setter(answers[1].lower()),
         "engine":"google",
         "safe":"active",
         "device":"desktop"
         }
     return params
-x = Courses_parameters()
-print(x)
 
-#t kullanıcının girdiği ülke verisi uygunmu diye control edilecek
+
