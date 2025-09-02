b1cbcc4cde6cf5a059e27ae9ff9f43cf1c161c28
AlgorithmXplorer <tanrininkirbaci36@gmail.com>
2025-09-02 23:42:09 +0300

    two file's name have been changed. trash codes in the main file have been deleted

--- main.py
+++ main.py
@@ -1,73 +1,10 @@
-from Immersive_Products import *
-from Jobs_API import *
 from file_funcs import *
 from parameter_bringer import *
+
+from Immersive_Products_api_service import *
+from Jobs_API_service import *
 from News_api_service import *
-from courses_API_service import *
 from Nutrition_Information_api_service import * 
 from Popular_Destinations_api_service import *
+from courses_API_service import *
 
-
-# params = Immersive_Products_parameters(param_taker=param_taker , domain_setter= domain_setter)
-# obje = Immersive_Products(parameters=params) 
-# obje.url_maker()
-# obje.data_taker()
-# datas = obje.clear_data_maker()
-# data_writer(datas=datas)
-
-# params = Jobs_parameters(param_taker=param_taker,domain_setter=domain_setter)
-# job_count = job_count(param_taker=param_taker)
-# obje = Jobs_apı(params=params,job_count=job_count)
-# obje.url_maker()
-# obje.data_taker()
-# obje.data_count_controller()
-# datas = obje.clear_data_maker()
-# data_writer(datas=datas)
-# print(obje.job_count)
-
-# params = News_parameters(param_taker=param_taker,domain_setter=domain_setter)
-# obje = news_api(params=params)
-# obje.url_maker()
-# obje.data_taker()
-# datas = obje.clear_data_maker()
-# data_writer(datas=datas)
-# print(obje.url)
-
-# params = Courses_param_taker(param_taker=param_taker,domain_setter=domain_setter)
-# count = course_count(param_taker=param_taker)
-# obje = courses_api(params=params,course_count=count)
-# obje.url_maker()
-# obje.data_taker()
-# obje.count_controller()
-# datas = obje.clear_data_maker()
-# data_writer(datas=datas)
-
-
-# params = Nutrition_Information_param_taker(param_taker=param_taker,domain_setter=domain_setter)
-# obje = Nutrition_Information_api(params=params)
-# obje.url_maker()
-# print(obje.url)
-# obje.datas_taker()
-# datas = obje.clear_data_maker()
-# data_writer(datas=datas,params=obje.params)
-
-
-# params = Popular_Destinations_parameters(param_taker=param_taker,domain_setter=domain_setter)
-# dest_count = count_Destination(param_taker=param_taker)
-# obje = Popular_Destinations_api(params=params , destination_count=dest_count)
-# obje.url_maker()
-# obje.data_taker()
-# obje.count_controller()
-# obje.data_count()
-# datas = obje.clear_data_maker()
-# data_writer(datas=datas)
-
-# print(len(obje.datas))
-# print(len(obje.datas))
-# print(obje.url)
-
-
-
-
-
-# data_writer(datas=datas,params=obje.params)
