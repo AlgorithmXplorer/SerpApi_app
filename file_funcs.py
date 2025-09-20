48739d8e450b43a9082c1abbfd948ac6f471b79a
AlgorithmXplorer <tanrininkirbaci36@gmail.com>
2025-09-20 16:28:27 +0300

    The decriptions of file_funcs file's functions have been completed

--- file_funcs.py
+++ file_funcs.py
@@ -3,8 +3,8 @@ import pygetwindow
 import json as js
 import time
 
-
-def domain_setter(code:str,only_country=False):
+#* A function that determines the country the user will use and makes it a parameter
+def domain_setter(code:str,only_country=False) -> str:
     with open("jsons/google-domains.json","r+",encoding="utf-8") as file:
         countrys = js.load(file)
 
@@ -17,17 +17,23 @@ def domain_setter(code:str,only_country=False):
               
 
 
-
+#* Purpose of this function: to take the incoming question list, get the answers through a file, and make them parameters.
 def param_taker(questions:list) -> list:
-    #* the question file creating and writing of questions
+    
+    #* The incoming question list is organized.
     questions = [ques + ":" for ques in questions]
 
+    #* The organized questions are written into the "questions.txt" file.
     with open("questions.txt","w",encoding="utf-8") as file:
         for question in questions:
             file.write(question+"\n")
         
         file.write("\nPlease write your filters correctly and dont forget save and close the file.\nAlso don't change any line in this file  except filter lines.")
+        #* This note is very important for the code to work.
+
     
+    #* Working logic of the "data_taker" function: it checks at very short intervals whether the file named "questions.txt" is closed.
+    #*  If the file has been closed, it takes the answers from each question line and outputs them.
     def data_taker():
         while True:
             windows:str = pygetwindow.getActiveWindowTitle()
@@ -40,25 +46,32 @@ def param_taker(questions:list) -> list:
         with open("questions.txt" ,"r+" , encoding="Utf-8") as file:
             question_lines = file.readlines()[:-3]
             
-            
         return question_lines
+    
     
     os.system("questions.txt")
     
-    #* data clearing
+    #* The output data is converted into parameters.
     question_lines = data_taker()
-    os.remove("questions.txt")
     question_lines = [line.split(":")[-1] for line in question_lines]
     question_lines = [line[:-1] for line in question_lines]
     question_lines = [line.lower() for line in question_lines]
     question_lines = [line.strip(" ") for line in question_lines]
     question_lines = [line if line != "" else None for line in question_lines ]
+    
+    #* Since the "questions.txt" file is unnecessary, it is deleted, but if it does not exist in that folder, the error is skipped.
+    try:
+        os.remove("questions.txt")
+    except FileNotFoundError:
+        pass
+
     return question_lines
 
 
-
-def data_writer(datas:str, params:dict):
-    #* gelen dataları oluşturulan dosyaya yazdırıcak
+#* Purpose of this function: to save the received data (outputs of the service functions) into a file and present it to the user.
+def data_writer(datas:str, params:dict) -> None:
+    
+    #* The parameters entered by the user are saved at the beginning of the file as "bot".
     str_params = [f"Searched topic = {params['q']}"]
     if "google_domain" in params.keys():
         str_params.append(f"Location = {params['google_domain']}")
@@ -66,11 +79,12 @@ def data_writer(datas:str, params:dict):
         str_params.append(f"Location = {params['gl']}")
     message = "*"*20 +"\n"+"PARAMS"+"\n"+"\n".join(str_params) +"\n"+ "*"*20 +"\n\n" 
 
+    #* A file named "temp_file.txt" is created, and the received data is saved into it.
     with open("temp_file.txt","w",encoding="utf-8") as file:
         file.write(message)
         file.write(datas)
     os.system("temp_file.txt")
-    
+    #!NOTE: THE REASON THIS FUNCTION WRITES THE RECEIVED DATA DIRECTLY IS THAT THE SERVICE FUNCTIONS ALREADY FORMAT AND OUTPUT THE DATA.  
 
 
 
